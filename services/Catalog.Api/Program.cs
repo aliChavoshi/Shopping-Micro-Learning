d93ed011fd31025ff26770e1aeb78c5d74f93a66
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-06-06 14:31:34 +0330

    Add Swagger support and update launch settings
    - Included `Swashbuckle.AspNetCore` package for API documentation.
    - Configured Swagger services in `Program.cs` for endpoint testing.
    - Updated `launchSettings.json` to modify application URL and add IIS Express profile.

--- services/Catalog.Api/Program.cs
+++ services/Catalog.Api/Program.cs
@@ -1,8 +1,15 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer(); // برای پشتیبانی از endpoint ها
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Catalog.Api", Version = "v1",Description = "Catalog API"});
+});
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -12,6 +19,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();                         // فعال‌سازی Swagger
+    app.UseSwaggerUI();                       // فعال‌سازی رابط گرافیکی Swagger UI
 }
 
 app.UseAuthorization();
