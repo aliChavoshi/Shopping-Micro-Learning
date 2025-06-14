31cf2a7a878ed5aeb5db48710d67e5f673494d5a
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-06-14 20:10:59 +0330

    get products by brands and types handler

---
+++ services/Catalog.Application/Commands/CreateProductCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using Catalog.Application.Responses;
+using MediatR;
+
+namespace Catalog.Application.Commands;
+
+public class CreateProductCommand : IRequest<ProductResponse>
+{
+
+}
+public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand,ProductResponse>
+{
+    public Task<ProductResponse> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ services/Catalog.Application/Queries/Products/GetProductsByTypeQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Queries.Products;
+
+public class GetProductsByTypeQuery : IRequest<IEnumerable<ProductResponse>>
+{
+    public string Type { get; set; }
+
+    public GetProductsByTypeQuery(string type)
+    {
+        Type = type;
+    }
+}
+
+public class GetProductsByTypeQueryHandler(IProductRepository productRepository, IMapper mapper)
+    : IRequestHandler<GetProductsByTypeQuery, IEnumerable<ProductResponse>>
+{
+    public async Task<IEnumerable<ProductResponse>> Handle(GetProductsByTypeQuery request,
+        CancellationToken cancellationToken)
+    {
+        var result = await productRepository.GetProductsByType(request.Type);
+        return mapper.Map<IEnumerable<ProductResponse>>(result);
+    }
+}
