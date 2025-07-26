ffd05e1e66fc73c877296db87e1bb0b04128dfae
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-07-26 19:26:54 +0330

    Enhance BasketController with new operations
    Updated BasketController to include methods for getting, creating, and deleting baskets using IMediator. Modified launchSettings.json to enable browser launch and set the launch URL to "swagger" for API documentation.

--- services/basket/Basket.Api/Controllers/BasketController.cs
+++ services/basket/Basket.Api/Controllers/BasketController.cs
@@ -1,8 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Basket.Application.Commands.CreateBasket;
+using Basket.Application.Commands.DeleteBasket;
+using Basket.Application.Queries.GetBasket;
+using Basket.Application.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Basket.Api.Controllers;
 
-public class BasketController : ApiController
+public class BasketController(IMediator mediator) : ApiController
 {
-    
+    [HttpGet("{userName}")]
+    public async Task<ActionResult<ShoppingCartResponse>> GetBasketByUserName(string userName,
+        CancellationToken cancellationToken)
+    {
+        var result = await mediator.Send(new GetBasketByUserNameQuery(userName), cancellationToken);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ShoppingCartResponse>> CreateBasket([FromBody] CreateBasketCommand request,
+        CancellationToken cancellationToken)
+    {
+        var result = await mediator.Send(request, cancellationToken);
+        return Ok(result);
+    }
+
+    [HttpDelete("{userName}")]
+    public async Task<ActionResult<bool>> DeleteBasket(string userName, CancellationToken cancellationToken)
+    {
+        return Ok(await mediator.Send(new DeleteBasketCommand(userName), cancellationToken));
+    }
 }
