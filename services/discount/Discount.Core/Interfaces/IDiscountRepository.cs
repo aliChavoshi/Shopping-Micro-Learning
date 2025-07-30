b18f82a00da0f5f252395a22cf516d4a94cda55b
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-07-30 20:19:04 +0330

    #S4 E3 Create entity of Coupon

---
+++ services/discount/Discount.Core/Interfaces/IDiscountRepository.cs
@@ -0,0 +1,13 @@
+﻿using Discount.Core.Entities;
+
+namespace Discount.Core.Interfaces;
+
+public interface IDiscountRepository
+{
+    Task<Coupon> GetDiscount(string productId);
+    Task<Coupon> GetDiscountByName(string productName);
+    Task<bool> CreateDiscount(Coupon coupon);
+    Task<bool> UpdateDiscount(Coupon coupon);
+    Task<bool> DeleteDiscount(string productId);
+    Task<bool> DeleteDiscountByName(string productName);
+}
---
+++ services/discount/Discount.Infrastructure/Services/DiscountRepository.cs
@@ -0,0 +1,37 @@
+﻿using Discount.Core.Entities;
+using Discount.Core.Interfaces;
+
+namespace Discount.Infrastructure.Services;
+
+public class DiscountRepository : IDiscountRepository
+{
+    public Task<Coupon> GetDiscount(string productId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Coupon> GetDiscountByName(string productName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> CreateDiscount(Coupon coupon)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateDiscount(Coupon coupon)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteDiscount(string productId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteDiscountByName(string productName)
+    {
+        throw new NotImplementedException();
+    }
+}
