e409d9353881ff4c0c14bd9ba7fc7167e7b8cf3e
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-08-04 18:52:19 +0330

    #S4 E9 GetDiscountByNameQueryHandler

---
+++ services/discount/Discount.Application/Queries/GetDiscountByNameQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Discount.Application.Protos;
+using Discount.Core.Interfaces;
+using Grpc.Core;
+using MediatR;
+
+namespace Discount.Application.Queries;
+
+public class GetDiscountByNameQuery : IRequest<CouponModel>
+{
+    public string ProductName { get; set; }
+
+    public GetDiscountByNameQuery(string productName)
+    {
+        ProductName = productName;
+    }
+}
+
+public class GetDiscountByNameQueryHandler(IDiscountRepository discountRepository, IMapper mapper)
+    : IRequestHandler<GetDiscountByNameQuery, CouponModel>
+{
+    public async Task<CouponModel> Handle(GetDiscountByNameQuery request, CancellationToken cancellationToken)
+    {
+        var entity = await discountRepository.GetDiscountByName(request.ProductName);
+        if (entity == null)
+            throw new RpcException(new Status(StatusCode.NotFound, $"Discount not found for {request.ProductName}"));
+        return mapper.Map<CouponModel>(entity);
+    }
+}
