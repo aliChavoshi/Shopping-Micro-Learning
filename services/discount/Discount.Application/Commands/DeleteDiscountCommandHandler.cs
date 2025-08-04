e7f1a82070f878d72f3de305530943e5f44bdd8d
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-08-04 19:12:42 +0330

    #S4 E11 DeleteDiscountCommandHandler and Update

---
+++ services/discount/Discount.Application/Commands/DeleteDiscountCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Discount.Core.Interfaces;
+using MediatR;
+
+namespace Discount.Application.Commands;
+
+public class DeleteDiscountCommand : IRequest<bool>
+{
+    public string ProductName { get; set; }
+
+    public DeleteDiscountCommand(string productName)
+    {
+        ProductName = productName;
+    }
+}
+
+public class DeleteDiscountCommandHandler(IDiscountRepository discountRepository)
+    : IRequestHandler<DeleteDiscountCommand, bool>
+{
+    public async Task<bool> Handle(DeleteDiscountCommand request, CancellationToken cancellationToken)
+    {
+        return await discountRepository.DeleteDiscountByName(request.ProductName);
+    }
+}
---
+++ services/discount/Discount.Application/Commands/UpdateDiscountCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Discount.Application.Protos;
+using Discount.Core.Entities;
+using Discount.Core.Interfaces;
+using MediatR;
+
+namespace Discount.Application.Commands;
+
+public class UpdateDiscountCommand : IRequest<CouponModel>
+{
+    public CouponModel CouponModel { get; set; }
+
+    public UpdateDiscountCommand(CouponModel couponModel)
+    {
+        CouponModel = couponModel;
+    }
+}
+
+public class UpdateDiscountCommandHandler(IDiscountRepository discountRepository, IMapper mapper)
+    : IRequestHandler<UpdateDiscountCommand, CouponModel>
+{
+    public async Task<CouponModel> Handle(UpdateDiscountCommand request, CancellationToken cancellationToken)
+    {
+        var coupon = mapper.Map<Coupon>(request.CouponModel);
+        await discountRepository.UpdateDiscount(coupon);
+        return request.CouponModel;
+    }
+}
