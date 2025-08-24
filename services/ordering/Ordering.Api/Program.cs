74b146e28ca93acf7da8b3852f9deb0b0c055b37
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-08-24 18:55:40 +0330

    refactor program.cs

--- services/ordering/Ordering.Api/Program.cs
+++ services/ordering/Ordering.Api/Program.cs
@@ -1,12 +1,23 @@
-﻿using Ordering.Application;
+﻿using Asp.Versioning;
+using Ordering.Application;
 using Ordering.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
+//Register Versioning
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ReportApiVersions = true;
+});
 builder.Services.AddOpenApi();
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer(); // برای پشتیبانی از endpoint ها
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Basket.Api", Version = "v1", Description = "Basket API" });
+});
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 
@@ -15,11 +26,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.MapOpenApi();
+    app.UseSwagger(); // فعال‌سازی Swagger
+    app.UseSwaggerUI(); // فعال‌سازی رابط گرافیکی Swagger UI
 }
 
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
