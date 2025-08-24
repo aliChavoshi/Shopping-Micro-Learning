e22c714d81c3b7bac1ffbd13a31fed5ede2ab303
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-08-24 12:50:56 +0330

    validators for Update Order Command

---
+++ services/ordering/Ordering.Application/Features/Validators/DeleteOrderCommandValidators.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Ordering.Application.Features.Command.DeleteOrder;
+
+namespace Ordering.Application.Features.Validators;
+
+public class DeleteOrderCommandValidators : AbstractValidator<DeleteOrderCommand>
+{
+    public DeleteOrderCommandValidators()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("{UserName} is required")
+            .GreaterThan(0)
+            .WithMessage("{Id} can not be -ve");
+    }
+}
---
+++ services/ordering/Ordering.Application/Features/Validators/UpdateOrderCommandValidators.cs
@@ -0,0 +1,45 @@
+﻿using FluentValidation;
+using Ordering.Application.Features.Command.UpdateOrder;
+
+namespace Ordering.Application.Features.Validators;
+
+public class UpdateOrderCommandValidators : AbstractValidator<UpdateOrderCommand>
+{
+    public UpdateOrderCommandValidators()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("{UserName} is required")
+            .GreaterThan(0)
+            .WithMessage("{Id} can not be -ve");
+
+        RuleFor(x => x.UserName)
+            .NotEmpty()
+            .WithMessage("{UserName} is required")
+            .NotNull()
+            .MaximumLength(70)
+            .WithMessage("{UserName} must not exceed 70 characters");
+
+        RuleFor(x => x.TotalPrice)
+            .NotEmpty()
+            .WithMessage("{TotalPrice} is required")
+            .GreaterThanOrEqualTo(1)
+            .WithMessage("قیمت باید بیشتر از 0 تومان باشد");
+
+        RuleFor(x => x.EmailAddress)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("{EmailAddress} is required");
+
+        RuleFor(x => x.FirstName)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("{FirstName} is required");
+
+        RuleFor(x => x.LastName)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("{LastName} is required");
+    }
+}
