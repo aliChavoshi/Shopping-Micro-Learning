1a5a1667933a3ca3b0c55d83a58266b0b38db94c
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-09-05 15:58:23 +0330

    Add event bus constants and checkout event classes
    - Introduced `EventBusConstant` for basket checkout event queue.
    - Removed `Events` folder from project structure.
    - Added `BaseIntegrationEvent` class with correlation ID and creation date.
    - Created `BasketCheckoutEvent` class inheriting from `BaseIntegrationEvent` with properties for user and payment details.

---
+++ Infrastructure/EventBus.Messages/Events/BaseIntegrationEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace EventBus.Messages.Events;
+
+public class BaseIntegrationEvent
+{
+    public string CorrelationId { get; set; }
+    public DateTime CreationDate { get; set; }
+
+    public BaseIntegrationEvent()
+    {
+        CorrelationId = Guid.NewGuid().ToString();
+        CreationDate = DateTime.Now;
+    }
+
+    public BaseIntegrationEvent(Guid correlationId, DateTime creationDate)
+    {
+        CorrelationId = correlationId.ToString();
+        CreationDate = creationDate;
+    }
+}
---
+++ Infrastructure/EventBus.Messages/Events/BasketCheckoutEvent.cs
@@ -0,0 +1,28 @@
+﻿namespace EventBus.Messages.Events;
+
+public class BasketCheckoutEvent : BaseIntegrationEvent
+{
+    //Identity
+    public Guid Guid { get; set; }
+    public string? UserName { get; set; }
+
+    //Customer
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? EmailAddress { get; set; }
+    public string? BuyerPhoneNumber { get; set; }
+
+    //Address
+    public string? AddressLine { get; set; }
+    public string? State { get; set; }
+    public string? City { get; set; }
+
+    // Payment
+    public decimal TotalPrice { get; set; }
+    public int PaymentMethod { get; set; }
+    public string? PaymentTrackingCode { get; set; } //bank
+
+    //Shipping
+    public string? TrackingCode { get; set; }
+    public string? ShippingTrackingCode { get; set; } //post 
+}
