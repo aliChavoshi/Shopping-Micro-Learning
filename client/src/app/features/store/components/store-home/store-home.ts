bb1712bab3da4f366553cf0346644a8f8e1facca
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-10-25 19:44:10 +0330

    add store home component

--- client/src/app/features/store/components/store-home/store-home.ts
+++ client/src/app/features/store/components/store-home/store-home.ts
@@ -1,15 +1,18 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { StoreService } from '../../services/store.service';
 import { IPaginate } from '../../../../shared/models/pagination';
 import { ICatalog } from '../../../../shared/models/products';
+import { Subscription } from 'rxjs';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-store-home',
-  imports: [],
+  imports: [DecimalPipe],
   templateUrl: './store-home.html',
   styleUrl: './store-home.css'
 })
 export class StoreHome implements OnInit {
+  // private subscriptions = new Subscription();
   paginate!: IPaginate<ICatalog>;
   private storeService = inject(StoreService);
 
@@ -17,8 +20,13 @@ export class StoreHome implements OnInit {
     this.getAllProducts();
   }
   getAllProducts() {
-    this.storeService.getAllProducts().subscribe(res => {
+    let sub$ = this.storeService.getAllProducts().subscribe(res => {
+      console.log("🚀 ~ StoreHome ~ getAllProducts ~ res:", res)
       this.paginate = res;
     })
+    // this.subscriptions.add(sub$);
   }
+  // ngOnDestroy(): void {
+  //   this.subscriptions.unsubscribe();
+  // }
 }
