ca30dc42f1415dc3fee42307518e382c13fdb60e
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-10-31 13:58:21 +0330

    add active class to the types and brand

--- client/src/app/features/store/components/filter-brands/filter-brands.ts
+++ client/src/app/features/store/components/filter-brands/filter-brands.ts
@@ -1,15 +1,21 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { StoreService } from '../../services/store.service';
+import { IBrand } from '../../../../shared/models/products';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-filter-brands',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './filter-brands.html',
   styleUrl: './filter-brands.css'
 })
 export class FilterBrands implements OnInit {
   store = inject(StoreService);
+  selectedItem?: IBrand = { id: '', name: '' };
   ngOnInit(): void {
     this.store.getAllBrands().subscribe();
+  }
+  selectItem(brand: IBrand) {
+    this.selectedItem = this.store.brands()?.find(x => x.id == brand.id);
   }
 }
--- client/src/app/features/store/components/filter-types/filter-types.ts
+++ client/src/app/features/store/components/filter-types/filter-types.ts
@@ -1,16 +1,22 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { StoreService } from '../../services/store.service';
+import { IType } from '../../../../shared/models/products';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-filter-types',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './filter-types.html',
   styleUrl: './filter-types.css'
 })
 export class FilterTypes implements OnInit {
   store = inject(StoreService);
+  selectedItem?: IType = { id: '', name: '' };
   ngOnInit(): void {
     this.store.getAllTypes().subscribe();
+  }
+  selectItem(id: string) {
+    this.selectedItem = this.store.types()?.find(x => x.id == id);
   }
 
 }
