cb3fd49a0d2c1884a801856096b1fda0c41def9a
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-11-05 19:10:07 +0330

    searching and pagination

--- client/src/app/features/store/components/store-home/store-home.ts
+++ client/src/app/features/store/components/store-home/store-home.ts
@@ -1,7 +1,7 @@
 import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { StoreService } from '../../services/store.service';
 import { ProductItem } from "../product-item/product-item";
-import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { PageChangedEvent, PaginationModule } from 'ngx-bootstrap/pagination';
 import { FilterTypes } from "../filter-types/filter-types";
 import { FilterBrands } from "../filter-brands/filter-brands";
 import { IType } from '../../models/products';
@@ -15,5 +15,24 @@ import { ProductParams } from '../../models/productParams';
 })
 export class StoreHome {
   store = inject(StoreService);
-
+  onPageChanged($event: PageChangedEvent) {
+    const params = {
+      ... this.store.params(), pageIndex: $event.page
+    };
+    this.store.setParams(params);
+  }
+  reset() {
+    const params = { ... this.store.params(), search: '' };
+    this.store.setParams(params);
+  }
+  filterProducts(value: string) {
+    const params = { ... this.store.params(), search: value };
+    this.store.setParams(params);
+  }
+  calculateEndCountPagination() {
+    const pageSize = this.store.params().pageSize;
+    const pageIndex = this.store.params().pageIndex;
+    const count = this.store.products()!.count
+    return pageSize * pageIndex > count ? count : (pageSize * pageIndex)
+  }
 }
