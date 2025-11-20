3a836f7d4a295ae9510167836866edb72ec9e3a6
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-11-20 13:03:30 +0330

    basket count in home page

--- client/src/app/shared/components/navbar/navbar.ts
+++ client/src/app/shared/components/navbar/navbar.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { BasketService } from '../../../features/basket/services/basket.service';
 
 @Component({
   selector: 'app-navbar',
   imports: [RouterModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
-  standalone : true
+  standalone: true
 })
 export class Navbar {
+  basket = inject(BasketService);
 
+  getBasketCount() {
+    return this.basket.basket()?.items.reduce((sum, item) => sum + item.quantity, 0);
+  }
 }
