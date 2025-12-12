63201db1a0c7a778f6fd5bac888c3c83266d5eba
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-12-12 14:46:36 +0330

    add item in basket in the UI part 2

--- client/src/app/features/store/components/product-detail/product-detail.component.ts
+++ client/src/app/features/store/components/product-detail/product-detail.component.ts
@@ -1,50 +1,62 @@
-import { AfterContentInit, Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { ICatalog } from '../../models/products';
 import { StoreService } from '../../services/store.service';
-import { AsyncPipe, DecimalPipe } from '@angular/common';
-import { Observable, tap } from 'rxjs';
-import { BreadcrumbService } from 'xng-breadcrumb'
+import { DecimalPipe } from '@angular/common';
 import { BasketService } from '../../../basket/services/basket.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { IBasketItem } from '../../../basket/models/basket';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css'],
-  imports: [RouterLink, DecimalPipe, AsyncPipe]
+  imports: [RouterLink, DecimalPipe]
 })
 export class ProductDetailComponent implements OnInit {
 
   private activatedRoute = inject(ActivatedRoute);
-  private bcService = inject(BreadcrumbService);
   basketService = inject(BasketService);
   store = inject(StoreService);
+  basketItem = signal<IBasketItem | null>(null);
   //properties
   productId!: string;
-  product!: Observable<ICatalog>;
+  product = toSignal(this.store.getProductById(this.activatedRoute.snapshot.params['id']), {
+    initialValue: null
+  });
 
   constructor() {
   }
 
   ngOnInit() {
     this.productId = this.activatedRoute.snapshot.params['id'];
-    this.getProductById(this.productId);
+    this.getItemFromBasket();
   }
   addToBasket() {
-    //TODO
-    this.product.subscribe(product => {
-      this.basketService.addItemToBasket(product)
-        .subscribe(res => {
-          console.log("🚀 ~ ProductDetailComponent ~ addToBasket ~ res:", res)
-        })
-    })
+    this.basketService.addItemToBasket(this.product()!)
+      .subscribe(res => {
+        if (res) {
+          let itemBasket = res.items.find(x => x.productId == this.productId);
+          if (itemBasket) {
+            this.basketItem.set(itemBasket);
+          }
+        }
+      })
   }
   public isInBasket() {
     const item = this.basketService.basket()?.items.find(x => x.productId === this.productId);
     return item !== undefined ? true : false;
   }
-  private getProductById(id: string) {
-    this.product = this.store.getProductById(id)
-      .pipe(tap(x => this.bcService.set('@productDetail', x.name)));
+  private getItemFromBasket() {
+    if (this.basketService.basket() &&
+      this.basketService.basket()?.items.some(x => x.productId === this.productId)) {
+      const item = this.basketService.basket()?.items.find(x => x.productId === this.productId!);
+      if (item) {
+        this.basketItem.set(item);
+      }
+    }
   }
+  // private getProductById(id: string) {
+  //   this.product = this.store.getProductById(id)
+  //     .pipe(tap(x => this.bcService.set('@productDetail', x.name)));
+  // }
 }
